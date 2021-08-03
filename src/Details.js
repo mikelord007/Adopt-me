9db779d11a25b966190c479056bfa312519f7b1d
agentmike007 <manujasan23@gmail.com>
2021-08-03 21:52:39 +0530

    made a Carousel component

--- src/Details.js
+++ src/Details.js
@@ -1,41 +1,49 @@
-import {Component} from 'react';
-import { withRouter } from 'react-router-dom';
+import { Component } from "react";
+import { withRouter } from "react-router-dom";
+import Carousel from "./Carousel";
 
 class Details extends Component {
-    constructor (){
-        super();
-        this.state = { loading:true};
+  state = { loading: true };
+
+  async componentDidMount() {
+    const res = await fetch(
+      `http://pets-v2.dev-apis.com/pets?id=${this.props.match.params.id}`
+    );
+    const json = await res.json();
+    this.setState(
+      Object.assign(
+        {
+          loading: false,
+        },
+        json.pets[0]
+      )
+    );
+  }
+
+  handleIndexClick(event){
+    this.setState({})
+  }
+
+  render() {
+    const { animal, breed, city, state, description, name, images } = this.state;
+
+    if (this.state.loading) {
+      return <h2>loading … </h2>;
     }
 
-    async componentDidMount (){
-        const res = await fetch (`http://pets-v2.dev-apis.com/pets?id=${this.props.match.params.id}`);
-        const json = await res.json();
-        this.setState(
-            Object.assign(
-            {
-                loading:false,
-            },
-            json.pets[0]
-            )
-        );
-    }
-    render() {
-        const { animal, breed, city , state, description , name } = this.state;
-
-        return (
-        <div className="details">
-            <div>
-                <h1>{name}</h1>
-                <h2>{`${animal}-${breed}-${city}-${state}`}</h2>
-                <button>Adopt {name}</button>
-                <p>{description}</p>
-            </div>
+    return (
+      <div className="details">
+        <Carousel images={images} />
+        <div>
+          <h1>{name}</h1>
+          <h2>{`${animal}-${breed}-${city}-${state}`}</h2>
+          <button>Adopt {name}</button>
+          <p>{description}</p>
         </div>
-        );
-    }
-
+      </div>
+    );
+  }
 }
-
 
 // const Details = () => {
 //     return <h2>Hello </h2>;
